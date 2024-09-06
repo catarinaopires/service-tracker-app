261967e9b937f26df256552651bbac14fb55a270
Catarina Pires <catarina.o.pires@hotmail.com>
2024-09-06 19:47:28 +0100

    Add upcoming service to home screen

--- ServiceTracker/app/(auth)/home.tsx
+++ ServiceTracker/app/(auth)/home.tsx
@@ -1,5 +1,8 @@
+import { AgendaItem, Service } from "@/components/AgendaItem";
 import { Colors } from "@/constants/Colors";
+import { getUpcomingService } from "@/db/services";
 import auth from "@react-native-firebase/auth";
+import { useEffect, useState } from "react";
 import {
   ColorSchemeName,
   StyleSheet,
@@ -10,13 +13,31 @@ import {
 
 const Home = () => {
   const colorScheme: ColorSchemeName = useColorScheme() ?? "light";
+  const [upcomingService, setUpcomingService] = useState<Service | null>(null);
 
   const user = auth().currentUser;
+
+  useEffect(() => {
+    getUpcomingService(user?.uid).then((service) => {
+      if (service.length === 0) {
+        setUpcomingService(null);
+        return;
+      }
+      setUpcomingService(service[0] as Service);
+    });
+  }, []);
+
   return (
     <View style={styles(colorScheme).container}>
       <Text style={{ color: Colors[colorScheme].onBackground }}>
         Welcome {user?.email}
       </Text>
+
+      <Text style={{ color: Colors[colorScheme].onBackground }}>
+        Upcoming service:
+      </Text>
+
+      <AgendaItem item={upcomingService} inAgenda={false} />
     </View>
   );
 };
