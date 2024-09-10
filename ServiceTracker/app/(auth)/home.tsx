591bb6b70aaf82a064ec5a4fe374d75f149869f9
Catarina Pires <catarina.o.pires@hotmail.com>
2024-09-10 18:43:23 +0100

    Improve home screen design

--- ServiceTracker/app/(auth)/home.tsx
+++ ServiceTracker/app/(auth)/home.tsx
@@ -1,10 +1,14 @@
 import { AgendaItem, Service } from "@/components/AgendaItem";
+import { HelloWave } from "@/components/HelloWave";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { Colors } from "@/constants/Colors";
 import { getUpcomingService } from "@/db/services";
 import auth from "@react-native-firebase/auth";
 import { useEffect, useState } from "react";
+
 import {
   ColorSchemeName,
+  Image,
   StyleSheet,
   Text,
   useColorScheme,
@@ -28,17 +32,27 @@ const Home = () => {
   }, []);
 
   return (
-    <View style={styles(colorScheme).container}>
-      <Text style={{ color: Colors[colorScheme].onBackground }}>
-        Welcome {user?.email}
-      </Text>
+    <ParallaxScrollView
+      headerBackgroundColor={Colors[colorScheme].surfaceVariant}
+      headerImage={
+        <Image
+          source={require("../../assets/images/logo.png")}
+          style={styles(colorScheme).logoImage}
+        />
+      }
+    >
+      <View style={styles(colorScheme).titleContainer}>
+        <Text style={styles(colorScheme).title}>Welcome {user?.email}!</Text>
+        <HelloWave />
+      </View>
 
-      <Text style={{ color: Colors[colorScheme].onBackground }}>
-        Upcoming service:
-      </Text>
+      {/* Upcoming service */}
+      <View style={styles(colorScheme).stepContainer}>
+        <Text style={styles(colorScheme).subtitle}>Upcoming service:</Text>
 
-      <AgendaItem item={upcomingService} inAgenda={false} />
-    </View>
+        <AgendaItem item={upcomingService} inAgenda={false} />
+      </View>
+    </ParallaxScrollView>
   );
 };
 
@@ -48,6 +62,35 @@ const styles = (colorScheme: ColorSchemeName) => {
   return StyleSheet.create({
     container: {
       backgroundColor: Colors[scheme].background,
+    },
+    title: {
+      fontSize: 32,
+      fontWeight: "bold",
+      lineHeight: 32,
+      color: Colors[scheme].onBackground,
+    },
+    subtitle: {
+      fontSize: 20,
+      fontWeight: "bold",
+      color: Colors[scheme].onBackground,
+    },
+    backgroundLogo: {
+      backgroundColor: Colors[scheme].primaryContainer,
+    },
+    logoImage: {
+      width: 150,
+      height: 150,
+      alignSelf: "center",
+      margin: 20,
+    },
+    titleContainer: {
+      flexDirection: "row",
+      alignItems: "center",
+      gap: 8,
+    },
+    stepContainer: {
+      gap: 8,
+      marginBottom: 8,
     },
   });
 };
